5bbf3ed2fd121b578c13fa1b3be3745c42ba79ac
RomanGlvac <xglvac@stuba.sk>
2022-09-28 20:32:00 +0200

    feat: add beer tap animation

--- app/src/main/java/com/example/android/androidbeer/MainActivity.kt
+++ app/src/main/java/com/example/android/androidbeer/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.android.androidbeer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.airbnb.lottie.LottieAnimationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<LottieAnimationView>(R.id.animationView).setOnClickListener {
+            playAnimation(it as LottieAnimationView)
+        }
+    }
+
+    private fun playAnimation(view : LottieAnimationView){
+        view.playAnimation()
     }
 }
