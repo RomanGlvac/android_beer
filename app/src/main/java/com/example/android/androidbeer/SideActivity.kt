d0ab42c24b99030ef6e087e999b8a1658160fa20
RomanGlvac <xglvac@stuba.sk>
2022-09-30 11:42:58 +0200

    feat: add base for second activity

---
+++ app/src/main/java/com/example/android/androidbeer/SideActivity.kt
@@ -0,0 +1,11 @@
+package com.example.android.androidbeer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SideActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_side)
+    }
+}
