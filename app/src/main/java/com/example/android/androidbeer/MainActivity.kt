c76c4f64acf3e094392d1ef5f91733e4239d9d0b
RomanGlvac <xglvac@stuba.sk>
2022-10-07 16:21:30 +0200

    Revert "Merge branch 'personal' into main"
    This reverts commit 9b8c22c8ca510312ddaf719ca1e507e291e37953, reversing
    changes made to 8d8f9514b2779ffca20aef71d5b7786d66b4aa63.

--- app/src/main/java/com/example/android/androidbeer/MainActivity.kt
+++ app/src/main/java/com/example/android/androidbeer/MainActivity.kt
@@ -1,12 +1,10 @@
 package com.example.android.androidbeer
 
 import android.animation.Animator
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.Button
 import android.widget.TextView
 import com.airbnb.lottie.LottieAnimationView
 import com.airbnb.lottie.model.KeyPath
