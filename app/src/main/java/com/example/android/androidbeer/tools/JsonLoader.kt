9e4af86fafcf44effaac341d582970d5534068ac
RomanGlvac <xglvac@stuba.sk>
2022-10-13 10:27:08 +0200

    style: rename variables in JsonLoader

--- app/src/main/java/com/example/android/androidbeer/tools/JsonLoader.kt
+++ app/src/main/java/com/example/android/androidbeer/tools/JsonLoader.kt
@@ -3,20 +3,18 @@ package com.example.android.androidbeer.tools
 import android.content.Context
 import android.util.Log
 import java.io.IOException
-import java.nio.charset.Charset
 
 class JsonLoader {
     companion object {
         fun openJson(context : Context, path: String) : String? {
             var json : String? = null
-            val charset : Charset = Charsets.UTF_8
             json = try {
-                val `is` = context.assets.open(path)
-                val size = `is`.available()
+                val inputStream = context.assets.open(path)
+                val size = inputStream.available()
                 val buffer = ByteArray(size)
-                `is`.read(buffer)
-                `is`.close()
-                String(buffer, charset)
+                inputStream.read(buffer)
+                inputStream.close()
+                String(buffer, Charsets.UTF_8)
             } catch (e : IOException){
                 Log.e("JsonLoader", "Failed to load JSON file: ${e.stackTrace}")
                 null
