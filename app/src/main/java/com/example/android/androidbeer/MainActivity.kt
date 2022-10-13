4eaa5f5f9354efa214b16d39346b0630c3051a49
RomanGlvac <xglvac@stuba.sk>
2022-10-13 21:11:04 +0200

    feat: add navigation drawer

--- app/src/main/java/com/example/android/androidbeer/MainActivity.kt
+++ app/src/main/java/com/example/android/androidbeer/MainActivity.kt
@@ -4,10 +4,14 @@ import android.animation.Animator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.View
-import android.widget.TextView
-import com.airbnb.lottie.LottieAnimationView
-import com.airbnb.lottie.model.KeyPath
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.android.androidbeer.databinding.ActivityMainBinding
 import com.example.android.androidbeer.models.PubHolder
 import com.example.android.androidbeer.tools.JsonLoader
@@ -16,15 +20,47 @@ import com.google.gson.Gson
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
+    private lateinit var navController : NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var listener : NavController.OnDestinationChangedListener
+
     private var _pubHolder : PubHolder? = null
     val pubHolder get() = _pubHolder!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupNavigationDrawer()
+        loadPubList()
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        var navController : NavController? = null
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView)
+        if (navHostFragment != null) {
+            navController = navHostFragment.findNavController()
+        }
+        if (navController != null) {
+            return navController.navigateUp(appBarConfiguration)
+        }
+        return super.onSupportNavigateUp()
+    }
+
+    private fun setupNavigationDrawer(){
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView)
+        if (navHostFragment != null) {
+            navController = navHostFragment.findNavController()
+        }
+        appBarConfiguration = AppBarConfiguration(navController.graph, binding.drawerLayout)
+
+        binding.navView.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    private fun loadPubList(){
         val json = JsonLoader.openJson(applicationContext, "pubs.json")!!
         _pubHolder = Gson().fromJson(json, PubHolder::class.java)
-        setContentView(binding.root)
     }
 
     override fun onPause() {
