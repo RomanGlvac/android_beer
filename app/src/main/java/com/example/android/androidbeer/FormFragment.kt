002b113d6e2e24611708ea41aefc79296a1242f2
RomanGlvac <xglvac@stuba.sk>
2022-10-13 20:00:19 +0200

    feat: remove navigation button in form fragment

--- app/src/main/java/com/example/android/androidbeer/FormFragment.kt
+++ app/src/main/java/com/example/android/androidbeer/FormFragment.kt
@@ -30,10 +30,6 @@ class FormFragment : Fragment() {
             )
             it.findNavController().navigate(action)
         }
-
-        binding.btnGotoRecycler.setOnClickListener {
-            it.findNavController().navigate(R.id.action_formFragment_to_recyclerFragment)
-        }
         return binding.root
     }
 
