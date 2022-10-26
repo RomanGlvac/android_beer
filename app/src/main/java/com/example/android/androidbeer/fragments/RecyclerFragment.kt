653980370823deed14575ca36b246e38eeebc70c
RomanGlvac <xglvac@stuba.sk>
2022-10-26 14:12:41 +0200

    feat: add retrofit service for pubs

--- app/src/main/java/com/example/android/androidbeer/fragments/RecyclerFragment.kt
+++ app/src/main/java/com/example/android/androidbeer/fragments/RecyclerFragment.kt
@@ -1,6 +1,7 @@
 package com.example.android.androidbeer.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -20,10 +21,13 @@ class RecyclerFragment : Fragment() {
     private var _binding : FragmentRecyclerBinding? = null
     private val binding get() = _binding!!
 
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        Log.d("RecyclerFragment", "onCreateView")
         _binding = FragmentRecyclerBinding.inflate(inflater, container, false)
         initRecyclerView(container!!)
         setButtonListeners()
