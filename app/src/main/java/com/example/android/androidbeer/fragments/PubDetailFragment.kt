274b429e8c557c143b4ce26e73befacc1a93f3d9
RomanGlvac <xglvac@stuba.sk>
2022-10-27 17:55:11 +0200

    feat: add delete functionality to pub detail

--- app/src/main/java/com/example/android/androidbeer/fragments/PubDetailFragment.kt
+++ app/src/main/java/com/example/android/androidbeer/fragments/PubDetailFragment.kt
@@ -5,16 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.navArgs
 import com.example.android.androidbeer.MainActivity
 import com.example.android.androidbeer.databinding.FragmentPubDetailBinding
+import com.example.android.androidbeer.models.PubModel
 import com.example.android.androidbeer.tools.PubManager
+import com.example.android.androidbeer.viewmodel.PubListViewModel
 
 
 class PubDetailFragment : Fragment() {
 
     private lateinit var binding : FragmentPubDetailBinding
     private val args : PubDetailFragmentArgs by navArgs()
+    private val viewModel : PubListViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -35,11 +39,12 @@ class PubDetailFragment : Fragment() {
         }
     }
 
+
     private fun initButton(){
         binding.btnDelete.setOnClickListener {
             val activity : MainActivity = this.activity as MainActivity
-            PubManager.getPubHolder(binding.root.context)!!.removePub(args.position)
-            activity.onBackPressed()
+            viewModel.deletePub(args.position)
+            activity.onBackPressedDispatcher.onBackPressed()
         }
     }
 
--- app/src/main/java/com/example/android/androidbeer/fragments/RecyclerFragment.kt
+++ app/src/main/java/com/example/android/androidbeer/fragments/RecyclerFragment.kt
@@ -6,8 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.android.androidbeer.databinding.FragmentRecyclerBinding
 import com.example.android.androidbeer.recyclerviews.PubAdapter
@@ -21,7 +23,7 @@ import java.util.*
 class RecyclerFragment : Fragment() {
 
     private lateinit var binding: FragmentRecyclerBinding
-    private val viewModel: PubListViewModel by viewModels()
+    private val viewModel: PubListViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,7 +34,20 @@ class RecyclerFragment : Fragment() {
         binding.viewModel = viewModel
         binding.rvPubs.adapter = PubAdapter(
             deleteListener = { position -> viewModel.deletePub(position) },
-            clickListener = { position -> Log.d("clickListener", "Clicked at $position") }
+            clickListener = { position ->
+                val pub = viewModel.pubHolder.value?.pubs?.get(position)
+                if(pub != null){
+                    val action = RecyclerFragmentDirections.actionRecyclerFragmentToPubDetailFragment(
+                        id = pub.id,
+                        name = pub.pubInfo.name,
+                        website = pub.pubInfo.website,
+                        operator = pub.pubInfo.operator,
+                        openingHours = pub.pubInfo.openingHours,
+                        position = position
+                    )
+                    findNavController().navigate(action)
+                }
+            }
         )
         binding.rvPubs.layoutManager = LinearLayoutManager(binding.root.context)
         binding.swipeRefreshLayout.setOnRefreshListener {
